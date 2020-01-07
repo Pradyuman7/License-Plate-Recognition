d082debba0a1dc8541745c2842e5f65968f5a896
pradyuman.dixit <p.dixit@student.tudelft.nl>
2020-01-07 05:01:25 +0100

    improve

--- temp.py
+++ temp.py
@@ -5,6 +5,31 @@ import pytesseract
 pytesseract.pytesseract.tesseract_cmd = "/usr/local/Cellar/tesseract/4.1.1/bin/tesseract"
 
 
+# Resizes a image and maintains aspect ratio
+def maintain_aspect_ratio_resize(image, width=None, height=None, inter=cv2.INTER_AREA):
+    # Grab the image size and initialize dimensions
+    dim = None
+    (h, w) = image.shape[:2]
+
+    # Return original image if no need to resize
+    if width is None and height is None:
+        return image
+
+    # We are resizing height if width is none
+    if width is None:
+        # Calculate the ratio of the height and construct the dimensions
+        r = height / float(h)
+        dim = (int(w * r), height)
+    # We are resizing width if height is none
+    else:
+        # Calculate the ratio of the 0idth and construct the dimensions
+        r = width / float(w)
+        dim = (width, int(h * r))
+
+    # Return the resized image
+    return cv2.resize(image, dim, interpolation=inter)
+
+
 # Load image, create mask, grayscale, Otsu's threshold
 def find(image):
     mask = np.zeros(image.shape, dtype=np.uint8)
@@ -27,4 +52,6 @@ def find(image):
 
     # Perfrom OCR with Pytesseract
     data = pytesseract.image_to_string(mask, lang='eng', config='--psm 6')
+    data = (data.encode('ascii', 'ignore')).decode("utf-8")
+    data = data.translate({ord(i):None for i in '!@#$'})
     print(data)
